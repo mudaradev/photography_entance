8e07f71c7014d474c56947f7f2fb3bfe05d881e6
mudaradev <146715349+mudaradev@users.noreply.github.com>
2025-01-30 02:58:43 +0530

    Create global.css

---
+++ global.css
@@ -0,0 +1,22 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+:root {
+  --color-navy: #1a237e;
+  --color-gold: #ffd700;
+}
+
+body {
+  font-family: var(--font-open-sans), sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: var(--font-montserrat), sans-serif;
+}
+
